f66ff6c87c37dc7254b864407a93613463a3d97c
suanity <baxayo@gmail.com>
2026-04-24 18:41:11 +0700

    Delete remind.js

--- remind.js
+++
@@ -1,6 +0,0 @@
-import { sendReminders } from './index.js';
-
-export default async function handler(req) {
-  const count = await sendReminders();
-  return new Response(`Sent to ${count} users`);
-}
